755d972b7a970376f5a67494fdc62db1007e6ef8
NAGARAJ S <nagaraj516700@gmail.com>
2024-05-19 22:35:29 +0530

    Feat : Get all patient by user

--- route/patientRoute.js
+++ route/patientRoute.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const { updatePatientDetails, getAllPatientDetails, getPatientDetailsById, getPatientDetailsByCenterId, getPatientDetailsByUserId } = require('../controller/patientController');
+const { 
+      updatePatientDetails, 
+      getAllPatientDetails, 
+      getPatientDetailsById, 
+      getPatientDetailsByCenterId, 
+      getPatientDetailsByUserId 
+   } = require('../controller/patientController');
 const { verifyAdmin, verifyUser } = require('../controller/authVerify');
 const router = express.Router();
 
 router.get('/',verifyAdmin,getAllPatientDetails);
-router.get('/center/:id',getPatientDetailsByCenterId);
-router.get('/user/:id',getPatientDetailsByUserId);
+router.get('/center/:id',verifyAdmin,getPatientDetailsByCenterId);
+router.get('/user/:id',verifyUser,getPatientDetailsByUserId);
 
 router.get('/:id',verifyUser,getPatientDetailsById);
 router.put('/:id',verifyAdmin,updatePatientDetails);
